5c709cbd23c6f78a6076fcacdb1f7b94adb0522b
Matias Wallenius <MatiWall@live.dk>
2025-02-28 21:13:06 +0100

    initial switch

--- stories/components/tabs/Switch.stories.tsx
+++ stories/components/tabs/Switch.stories.tsx
@@ -1,41 +1,38 @@
 import React from 'react'
 import { RoutingSwitch } from '../../../src'
-import { BrowserRouter, Route, Routes, Outlet, useLocation } from 'react-router-dom'
-import { Meta, StoryObj } from '@storybook/react';
+import { MemoryRouter, Route, Routes, Outlet, useLocation } from 'react-router-dom'
+import { Meta, StoryObj, StoryFn } from '@storybook/react';
 
 export default {
-    title: 'components/tabs/RoutingSwitch',
-    component: RoutingSwitch,
-    decorators: [
-        (Story) => (
-            <BrowserRouter>
-                <Story />
-            </BrowserRouter>
-        )
-    ]
-} as Meta
+  title: "Components/RoutingSwitch",
+  component: RoutingSwitch,
+  decorators: [
+    (Story) => (
+      <MemoryRouter initialEntries={["/home"]}>
+        <Routes>
+          <Route path="/" element={<Story />}>
+            <Route path="home" element={<div>🏠 Home Page</div>} />
+            <Route path="about" element={<div>ℹ️ About Page</div>} />
+            <Route path="contact" element={<div>📞 Contact Page</div>} />
+            <Route path="disabled" element={<div>Disabled</div>} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    ),
+  ],
+} as Meta<typeof RoutingSwitch>;
 
+const Template: StoryFn<typeof RoutingSwitch> = () => (
+  <div>
+    <RoutingSwitch>
+      <RoutingSwitch.Item label="Home" value="/home" />
+      <RoutingSwitch.Item label="About" value="/about" />
+      <RoutingSwitch.Item label="Contact" value="/contact" />
+      <RoutingSwitch.Item label="Disabled" value="/disabled" active={false} />
+    </RoutingSwitch>
+    <Outlet />
+  </div>
+);
 
+export const Default = Template.bind({});
 
-
-export const Item: StoryObj<typeof RoutingSwitch.Item> = {
-    render: () => <RoutingSwitch.Item key={'/test'} label={'Test'} path={'/'} active={true}></RoutingSwitch.Item>
-}
-
-
-export const Switch: StoryObj<typeof RoutingSwitch> = {
-    args: { },
-    render: (args) => {
-        const location = useLocation();
-        return (<>
-            <RoutingSwitch>
-                <RoutingSwitch.Item key={'/'} label={'Test'} value={'/'}></RoutingSwitch.Item >
-                <RoutingSwitch.Item key={'/test2'} label={'Test2'} value={'/test2'}></RoutingSwitch.Item >
-                <RoutingSwitch.Item key={'/test3'} label={'False'} value={'/test3'} active={false}></RoutingSwitch.Item >
-                <RoutingSwitch.Item key={'/test4'} label={'Test 4'} value={'/test4'} active={true}></RoutingSwitch.Item >
-            </RoutingSwitch>
-            <Outlet/>
-        </>
-        )
-    }
-}
